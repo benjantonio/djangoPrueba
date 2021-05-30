2a05b50faf6dfd79a736007c0693e6d269b5729d
Benjamín Navarro <be.navarroa@duocuc.cl>
2021-05-30 18:42:05 -0400

    Incorporación base de datos y usuario admin

--- HCabrera/productos/admin.py
+++ HCabrera/productos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Maquina
+from .models import Repuesto
 
 # Register your models here.
+
+admin.site.register(Maquina)
+admin.site.register(Repuesto)
