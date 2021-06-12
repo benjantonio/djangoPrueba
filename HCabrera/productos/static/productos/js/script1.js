ff670786cb89a75c269ffc7eda5eb2fbd4707154
FranOrtega <franc.ortegac@duocuc.cl>
2021-06-12 19:17:22 -0400

    Modificacipon botones

--- HCabrera/productos/static/productos/js/script1.js
+++
@@ -1,9 +0,0 @@
-<script>
-$( document ).ready(function() {
-    $("#id_idRepuesto").attr("class","app-form-control");
-    $("#id_nombreRepuesto").attr("class","app-form-control");
-    $("#id_maquina").attr("class","form-control");
-    $("#id_descripcionRepuesto").attr("class","app-form-control");
-  
-  });
-</script>
