efe0525fd2af6cc85cd1770e495800f7165e4315
FranOrtega <franc.ortegac@duocuc.cl>
2021-06-12 18:55:07 -0400

    Js formulario agregar

---
+++ HCabrera/productos/static/productos/js/script1.js
@@ -0,0 +1,9 @@
+<script>
+$( document ).ready(function() {
+    $("#id_idRepuesto").attr("class","app-form-control");
+    $("#id_nombreRepuesto").attr("class","app-form-control");
+    $("#id_maquina").attr("class","form-control");
+    $("#id_descripcionRepuesto").attr("class","app-form-control");
+  
+  });
+</script>
