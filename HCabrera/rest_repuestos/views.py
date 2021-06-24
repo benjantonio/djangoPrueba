3db89a5f96ba09bc36c6df5f1c1a8ee24dbec65a
Benjamín Navarro <be.navarroa@duocuc.cl>
2021-06-24 10:07:38 -0400

    cambios

--- HCabrera/rest_repuestos/views.py
+++ HCabrera/rest_repuestos/views.py
@@ -22,3 +22,4 @@ def lista_repuestos(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
