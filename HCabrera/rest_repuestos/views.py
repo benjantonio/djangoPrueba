f885503f134da62709dd8accf4fc42367a2301c6
Benjamín Navarro <be.navarroa@duocuc.cl>
2021-06-23 23:11:50 -0400

    Creación Serializer

--- HCabrera/rest_repuestos/views.py
+++ HCabrera/rest_repuestos/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
+from productos.models import Repuesto
+from .serializers import RepuestoSerializer
 
-# Create your views here.
+@csrf_exempt
+@api_view(['GET','POST'])
+
+def lista_repuestos(request):
+    if request.method == 'GET':
+        repuesto = Repuesto.objects.all()
+        serializer = RepuestoSerializer(repuesto, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = RepuestoSerializer(data=data)
+        if(serializer.is_valid()):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
